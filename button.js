428544e0e452bc4de0f8ac90b12a3f4b489ef587
skysystembold <114417760+skysystembold@users.noreply.github.com>
2025-03-05 11:11:27 -0500

    Update button.js

--- button.js
+++ button.js
@@ -1,4 +1,1 @@
-const informacion = document.querySelector(".informacion")
-const closeX = document.querySelector("#closex")
-closeX.addEventListener("click", () => {
-informacion.style.display = "none"
+
