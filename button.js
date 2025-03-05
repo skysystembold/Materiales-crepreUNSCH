cdaab69fe347830a22b382e10e59f03413303123
skysystembold <114417760+skysystembold@users.noreply.github.com>
2025-03-05 11:04:35 -0500

    Create button.js

---
+++ button.js
@@ -0,0 +1,4 @@
+const informacion = document.querySelector(".informacion")
+const closeX = document.querySelector("#closeX")
+closeX.addEventListener("click", () => {
+informacion.style.display = "none"
